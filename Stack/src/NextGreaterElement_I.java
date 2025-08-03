2959c68f19f897c100ee7e38fae5671d24f59b7e
theraghulr <rraghul1382@gmail.com>
2025-08-03 22:26:14 +0530

    Fixing Stack

--- Stack/src/NextGreaterElement_I.java
+++ Stack/src/NextGreaterElement_I.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class NextGreaterElement_I {
+    public static void main(String[] args) {
+        int[] nums1 = {4, 1, 2};
+        int[] nums2 = {1, 3, 4, 2};
+
+        NextGreaterElement_I obj = new NextGreaterElement_I();
+        int[] result = obj.nextGreaterElement(nums1, nums2);
+
+        System.out.println("Next Greater Elements: " + Arrays.toString(result));
+    }
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> nextGreaterMap = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        // Traverse nums2 in reverse to fill map
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            int current = nums2[i];
+
+            while (!stack.isEmpty() && stack.peek() <= current) {
+                stack.pop();
+            }
+
+            nextGreaterMap.put(current, stack.isEmpty() ? -1 : stack.peek());
+
+            stack.push(current);
+        }
+
+        // Build result for nums1 from map
+        int[] result = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = nextGreaterMap.get(nums1[i]);
+        }
+
+        return result;
+    }
 }
+
--- Stack/src/NextGreaterElement_II.java
+++ Stack/src/NextGreaterElement_II.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class NextGreaterElement_II {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 3};
+        int[] result = nextGreaterElements(nums);
+
+        System.out.println("Next Greater Elements: " + Arrays.toString(result));
+    }
+
+    public static int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        Stack<Integer> stack = new Stack<>();  // stores indices
+        Arrays.fill(result, -1);
+
+        for (int i = 0; i < 2 * n; i++) {
+            int num = nums[i % n];
+            while (!stack.isEmpty() && nums[stack.peek()] < num) {
+                int index = stack.pop();
+                result[index] = num;
+            }
+            if (i < n) {
+                stack.push(i);
+            }
+        }
+        return result;
+    }
 }
+

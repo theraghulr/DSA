9fbefb58a5057f5273f44c4659c4faead59e7192
theraghulr <rraghul1382@gmail.com>
2025-08-05 19:13:52 +0530

    LinkedList

---
+++ LInkedList/src/CheckPalidrome.java
@@ -0,0 +1,41 @@
+public class CheckPalidrome {
+
+        public boolean isPalindrome(Node head) {
+        if (head == null || head.next == null) return true;
+
+        // Step 1: Find the middle
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Step 2: Reverse second half
+        Node secondHalf = reverseList(slow);
+
+        // Step 3: Compare both halves
+        Node firstHalf = head;
+        Node temp = secondHalf;
+        while (temp != null) {
+            if (firstHalf.data != temp.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            temp = temp.next;
+        }
+
+        return true;
+    }
+
+        private Node reverseList(Node head) {
+        Node prev = null;
+        while (head != null) {
+            Node next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
+    }
+
+}
